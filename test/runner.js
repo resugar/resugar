2a2c93a68ec5f8b397626669a04a0c87f81261c9
Brian Donovan <donovan@squareup.com>
2014-04-21 17:05:06 -0700

    Enhance the test runner.
    * Allow running a single example file.
    * Add `compile`/`normalize` helper functions to the example context.

--- test/runner.js
+++ test/runner.js
@@ -6,7 +6,10 @@
 
 Error.stackTraceLimit = 20;
 
-var compile = require('../lib').compile;
+var esnext = require('../lib');
+var recast = require('recast');
+var esprima = require('esprima');
+
 var fs = require('fs');
 var path = require('path');
 var RESULTS = 'test/results';
@@ -15,13 +18,27 @@ if (!fs.existsSync(RESULTS)) {
   fs.mkdirSync(RESULTS);
 }
 
-require('example-runner').runCLI(function(source, testName, filename) {
-  var result = compile(source, {
-    includeRuntime: true,
-    sourceFileName: filename,
-    sourceMapName: filename + '.map'
-  });
-  fs.writeFileSync(path.join(RESULTS, testName + '.js'), result.code, 'utf8');
-  fs.writeFileSync(path.join(RESULTS, testName + '.js.map'), JSON.stringify(result.map), 'utf8');
-  return result.code;
+require('example-runner').runCLI(process.argv.slice(2), {
+  transform: function(source, testName, filename) {
+    var result = esnext.compile(source, {
+      includeRuntime: true,
+      sourceFileName: filename,
+      sourceMapName: filename + '.map'
+    });
+    fs.writeFileSync(path.join(RESULTS, testName + '.js'), result.code, 'utf8');
+    fs.writeFileSync(path.join(RESULTS, testName + '.js.map'), JSON.stringify(result.map), 'utf8');
+    return result.code;
+  },
+
+  context: {
+    compile: function(source, options) {
+      return esnext.compile(source, options).code;
+    },
+
+    normalize: function(source) {
+      return recast.prettyPrint(
+        recast.parse(source, { esprima: esprima })
+      ).code;
+    }
+  }
 });
