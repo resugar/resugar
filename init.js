9825ca1af3ba87846cdc65ca1df67cb825181787
Brian Donovan <donovan@squareup.com>
2014-06-13 21:17:24 -0400

    Add console output to the demo page and actually run the code.

--- init.js
+++ init.js
@@ -1,36 +1,80 @@
-//var Compiler = ModuleTranspiler.Compiler,
-//  $input = $('#input'),
-//  $output = $('#output'),
-//  $command = $('#command'),
-//  $types = $('input[type=radio][name=type]'),
-//  waitTimeout;
-//
-//$input.keyup(function() {
-//  if (waitTimeout) clearTimeout(waitTimeout);
-//  waitTimeout = setTimeout(transpile, 100);
-//});
-//
-//$types.change(transpile);
-//
-//function transpile() {
-//  var output,
-//    type = $types.filter(':checked').attr('value');
-//
-//  try {
-//    var compiler = new Compiler($input.val(), 'segment', {imports: []});
-//    output = (type === 'amd') ? compiler.toAMD() : compiler.toCJS();
-//  } catch (ex) {
-//    output = ex.message + "\n" + ex.backtrace.join("\n");
-//  }
-//
-//  $command.text('$ compile-modules --type ' + type + ' --to output input');
-//  $output.val(output);
-//}
-
-//transpile();
+/* jshint evil:true */
 
 (function() {
+  function makeElementWithClass(className) {
+    var element = document.createElement('div');
+    element.className = className;
+    return element;
+  }
+
+  function Console(element) {
+    this._container = element;
+    this._groups = [element];
+  }
+  Console.prototype.log = function() {
+    this.append('log', [].slice.call(arguments));
+    console.log.apply(console, arguments);
+  };
+  Console.prototype.info = function() {
+    this.append('info', [].slice.call(arguments));
+    console.info.apply(console, arguments);
+  };
+  Console.prototype.warn = function() {
+    this.append('warn', [].slice.call(arguments));
+    console.warn.apply(console, arguments);
+  };
+  Console.prototype.error = function() {
+    this.append('error', [].slice.call(arguments));
+    console.error.apply(console, arguments);
+  };
+  Console.prototype.group = function() {
+    var group = makeElementWithClass('group');
+    this._currentGroup().appendChild(group);
+    this._groups.push(group);
+  };
+  Console.prototype.groupEnd = function() {
+    var groups = this._groups;
+    if (groups.length === 1) {
+      return;
+    }
+    groups.pop();
+  };
+  Console.prototype._flushGroups = function() {
+    this._groups = [this._groups[0]];
+  };
+  Console.prototype.append = function(level, args) {
+    var scrolledToBottom = this._isScrolledToBottom();
+    var lineElement = makeElementWithClass('line ' + level);
+    lineElement.appendChild(document.createTextNode(
+      args.map(function(item) {
+        return ''+item;
+      }).join(' ')
+    ));
+    this._currentGroup().appendChild(lineElement);
+    if (scrolledToBottom) {
+      this._container.scrollTop = this._container.scrollTopMax;
+    }
+  };
+  Console.prototype._currentGroup = function() {
+    return this._groups[this._groups.length - 1];
+  };
+  Console.prototype._isScrolledToBottom = function() {
+    return this._container.scrollTopMax < this._container.scrollTop + 10;
+  };
+
+  function Context(console) {
+    this.console = console;
+  }
+  Context.prototype.evaluate = function(source) {
+    var fn = new Function('console', source);
+    fn(this.console);
+  };
+
   var source = decodeURIComponent(location.hash.slice(1));
+  var context = new Context(
+    new Console(document.querySelector('.console'))
+  );
+
   [].forEach.call(document.querySelectorAll('.input-output'), function(pair) {
     var input = CodeMirror.fromTextArea(pair.querySelector('textarea:nth-of-type(1)'), {
       lineNumbers: true,
@@ -63,14 +107,26 @@
     });
 
     function convert() {
+      var compiled;
       try {
+        context.console._flushGroups();
         source = input.getValue();
         location.hash = encodeURIComponent(source);
-        output.setValue(esnext.compile(source).code);
+        compiled = esnext.compile(source).code;
+        context.console.group();
+        output.setValue(compiled);
         hideError();
       } catch (ex) {
-        console.log('conversion error: ', ex);
+        console.error('compile error:', ex);
         showError(ex.message);
+      }
+      if (compiled) {
+        try {
+          context.evaluate(compiled);
+        } catch (ex) {
+          console.error('runtime error:', ex);
+          showError(ex.message);
+        }
       }
     }
 
