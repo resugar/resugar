409981c17b36ee12fea1eef4946506887909e2eb
Brian Donovan <me@brian-donovan.com>
2016-01-05 09:41:18 -0800

    Really remove npm plugin usage.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,8 @@
 import babel from 'rollup-plugin-babel';
 
-function jsnextPlugin(packages) {
-  const npmInstance = npm({ jsnext: true });
-
-  return {
-    resolveId(importee, importer) {
-      if (packages.indexOf(importee) >= 0) {
-        return npmInstance.resolveId(`${importee}-jsnext`, importer);
-      }
-    }
-  };
-}
-
 export default {
   entry: 'src/esnext.js',
   plugins: [
-    jsnextPlugin(['assert']),
     babel({
       babelrc: false,
       presets: ['es2015-rollup'],
