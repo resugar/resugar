d8fc8bd36fb83161564020d1ad298762752a35f2
Brian Donovan <me@brian-donovan.com>
2016-03-28 20:53:53 -0700

    Read babel config from .babelrc.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,22 @@
 import babel from 'rollup-plugin-babel';
+import { readFileSync } from 'fs';
+
+function babelConfig() {
+  var result = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
+  result.babelrc = false;
+  result.presets = result.presets.map(function(preset) {
+    if (preset === 'es2015') {
+      return 'es2015-rollup';
+    } else {
+      return preset;
+    }
+  });
+  return result;
+}
 
 export default {
   entry: 'src/esnext.js',
   plugins: [
-    babel({
-      babelrc: false,
-      presets: ['es2015-rollup'],
-      plugins: ['syntax-flow', 'transform-flow-strip-types']
-    })
+    babel(babelConfig())
   ]
 };
