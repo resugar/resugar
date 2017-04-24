1eb16d7e7662616ac039e68f292766e987eab496
Alan Pierce <alangpierce@gmail.com>
2017-04-24 05:35:29 -0700

    feat: add an option to force a single default export (#127)
    This is useful for code that does complex modifications to `module.exports` and
    where we still want to JS modules. Rather than changing things to exports
    line-by-line, we declare an empty exports object at the top, then do the normal
    modifications, then `export default` it at the bottom.

---
+++ test/form/modules.commonjs/converts-complex-forced-default-export/main.js
@@ -0,0 +1,14 @@
+console.log('Start of file');
+module.exports = a;
+if (b) {
+  this.d = e;
+}
+for (let f in exports) {
+  module.exports[f]++;
+}
+exports[g] = {
+  h() {
+    console.log(this);
+  }
+};
+console.log('End of file');
