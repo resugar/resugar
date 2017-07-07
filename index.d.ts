13ce60edcb61f1a184d41cc0f83c3b116a1f3924
Alan Pierce <alangpierce@gmail.com>
2017-07-07 09:48:53 -0700

    fix: add basic TypeScript types (#132)
    I left off the `Plugin.visitor` method since it had a complex type that isn't
    necessary for any decaffeinate usages, but it would be needed for any
    externally-specified plugins.

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+import { Node, NodePath } from 'babel-traverse';
+
+declare module "esnext" {
+  type Plugin = {
+    name: string,
+    description: string,
+  };
+
+  type DeclarationsBlockScopeOptions = {
+    disableConst?: boolean | ((path: NodePath<Node>) => boolean),
+  };
+
+  type ModulesCommonJSOptions = {
+    forceDefaultExport?: boolean,
+    safeFunctionIdentifiers?: Array<string>,
+  };
+
+  type Options = {
+    plugins?: Array<Plugin>,
+    validate?: boolean,
+    'declarations.block-scope'?: DeclarationsBlockScopeOptions,
+    'modules.commonjs'?: ModulesCommonJSOptions,
+  };
+
+  type Warning = {
+    node: Node,
+    type: string,
+    message: string
+  };
+
+  type RenderedModule = {
+    ast: Node,
+    code: string,
+    metadata: Object,
+    warnings: Array<Warning>,
+  };
+
+  export function run(args: Array<string>): void;
+  export const allPlugins: Array<Plugin>;
+  export function convert(source: string, options?: Options): RenderedModule;
+}
