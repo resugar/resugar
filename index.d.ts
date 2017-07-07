367cba1cfdc9d05aeaba9038d1170eaa4c9177b0
Alan Pierce <alangpierce@gmail.com>
2017-07-07 12:37:59 -0700

    fix: clean up typescript definitions, update magic-string (#134)
    We shouldn't use `declare module` for library definitions like this (even though
    it usually works).

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,39 @@
 import { Node, NodePath } from 'babel-traverse';
 
-declare module "esnext" {
-  type Plugin = {
-    name: string,
-    description: string,
-  };
+type Plugin = {
+  name: string,
+  description: string,
+};
 
-  type DeclarationsBlockScopeOptions = {
-    disableConst?: boolean | ((path: NodePath<Node>) => boolean),
-  };
+type DeclarationsBlockScopeOptions = {
+  disableConst?: boolean | ((path: NodePath<Node>) => boolean),
+};
 
-  type ModulesCommonJSOptions = {
-    forceDefaultExport?: boolean,
-    safeFunctionIdentifiers?: Array<string>,
-  };
+type ModulesCommonJSOptions = {
+  forceDefaultExport?: boolean,
+  safeFunctionIdentifiers?: Array<string>,
+};
 
-  type Options = {
-    plugins?: Array<Plugin>,
-    validate?: boolean,
-    'declarations.block-scope'?: DeclarationsBlockScopeOptions,
-    'modules.commonjs'?: ModulesCommonJSOptions,
-  };
+type Options = {
+  plugins?: Array<Plugin>,
+  validate?: boolean,
+  'declarations.block-scope'?: DeclarationsBlockScopeOptions,
+  'modules.commonjs'?: ModulesCommonJSOptions,
+};
 
-  type Warning = {
-    node: Node,
-    type: string,
-    message: string
-  };
+type Warning = {
+  node: Node,
+  type: string,
+  message: string
+};
 
-  type RenderedModule = {
-    ast: Node,
-    code: string,
-    metadata: Object,
-    warnings: Array<Warning>,
-  };
+type RenderedModule = {
+  ast: Node,
+  code: string,
+  metadata: Object,
+  warnings: Array<Warning>,
+};
 
-  export function run(args: Array<string>): void;
-  export const allPlugins: Array<Plugin>;
-  export function convert(source: string, options?: Options): RenderedModule;
-}
+export function run(args: Array<string>): void;
+export const allPlugins: Array<Plugin>;
+export function convert(source: string, options?: Options): RenderedModule;
