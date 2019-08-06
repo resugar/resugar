020100c4d99910d894d4ab1c46eda65476dfbb32
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-08-06 16:47:31 -0700

    feat: add `script/run` to run codemods on stdin

---
+++ script/_commands/run.ts
@@ -0,0 +1,52 @@
+import codemodDeclarationsBlockScope from '../../packages/resugar-codemod-declarations-block-scope';
+import codemodFunctionsArrow from '../../packages/resugar-codemod-functions-arrow';
+import codemodModulesCommonJS from '../../packages/resugar-codemod-modules-commonjs';
+import codemodObjectsConcise from '../../packages/resugar-codemod-objects-concise';
+import codemodObjectsDestructuring from '../../packages/resugar-codemod-objects-destructuring';
+import codemodObjectsShorthand from '../../packages/resugar-codemod-objects-shorthand';
+import codemodStringsTemplate from '../../packages/resugar-codemod-strings-template';
+import { transform } from '@codemod/core';
+
+export default async function main(
+  args: ReadonlyArray<string>,
+  stdin: typeof process.stdin,
+  stdout: typeof process.stdout,
+  stderr: typeof process.stderr
+): Promise<number> {
+  const input = await readStream(stdin);
+  stdout.write(transform(input, {
+    plugins: [
+      codemodDeclarationsBlockScope,
+      codemodFunctionsArrow,
+      codemodModulesCommonJS,
+      codemodObjectsConcise,
+      codemodObjectsDestructuring,
+      codemodObjectsShorthand,
+      codemodStringsTemplate
+    ]
+  }).code as string);
+  return 0;
+}
+
+async function readStream(
+  input: NodeJS.ReadStream,
+  encoding = 'utf8'
+): Promise<string> {
+  return new Promise(resolve => {
+    let data = '';
+
+    input.setEncoding(encoding);
+
+    input.on('readable', () => {
+      let chunk;
+
+      while ((chunk = input.read()) !== null) {
+        data += chunk;
+      }
+    });
+
+    input.on('end', () => {
+      resolve(data);
+    });
+  });
+}
